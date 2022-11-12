3e4f2b4b8e131482df80be4e71c1d3a037b031ef
nbalamur03 <nbalamur@asu.edu>
2022-11-12 10:16:26 -0700

    Merge branch 'main' of https://github.com/spabolu/sundevil_pizza

--- src/application/firstPageController.java
+++ src/application/firstPageController.java
@@ -5,13 +5,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.awt.event.ActionEvent;
+import javafx.event.ActionEvent;
 import java.io.IOException;
 
 import java.net.URL;
@@ -19,17 +21,22 @@ import java.util.ResourceBundle;
 
 public class firstPageController implements Initializable{
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     //Textbox chef and student name 
     @FXML
-    private TextField studentName;
+    private TextField studentName = new TextField();
     @FXML
-    private TextField chefName;
+    private TextField chefName = new TextField();
 
     //ID fields used for chef and student 
     @FXML
-    private PasswordField studentIDEntered;
+    private PasswordField chefIDEntered = new PasswordField();
     @FXML
-    private PasswordField chefIDEntered;
+    private PasswordField studentIDEntered = new PasswordField();
+    
 
     //student and chef side button
     @FXML
@@ -39,55 +46,127 @@ public class firstPageController implements Initializable{
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
-        System.out.println("hi");
+        //inititialize
+
     }
 
     //student ID array - ins
-    int[] studentIdDatabase = {0000000000, 0000000001, 0000000002, 0000000003};
+    int[] studentIdDatabase = {1223574645, 1223453645, 1223453798, 1223674138};
 
     //chef ID array
-    int[] chefIdDatabase = {0000000000, 0000000001, 0000000002, 0000000003};
+    int[] chefIdDatabase = {1113574923, 1119357623, 1117499023, 1112274923};
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
 
-    //****************************************
+    
+    //****************************************************************************
     //if the student side "next" button is clicked
     //check if ID is entered, is a 10 digit integer and is in the student database
-    public void clickStudentNext(){
-        //check if name is entered
-        String studentID = studentIDEntered.getId();
-
-        //check if studentID entry is the in the studentIdDatabase array
-
-    }
-
-    //****************************************
-    //if the chef side "next" button is clicked
-    //check if ID is entered, is a 10 digit integer and is in the chef database
-    public void clickChefNext(){
-        //check if name is entered
-        //check if chefID entry is the in the chefIdDatabase array
-
-    }
-
-    public void switchToScene1(ActionEvent event) throws IOException 
+    @FXML 
+    public void goToStudent(ActionEvent event) throws IOException 
     {
+
+        boolean checkVal = false; //
+        boolean containsChar = false; //set to true if ID contains characters
+        String sName = studentName.getText();
+        int studentIdAsNum = 0;
+        String studentIDString = studentIDEntered.getText();
+
+
+        System.out.println(sName);
+        System.out.println(studentIDString);
+
+        try{ //parse the string input from password field to an int
+            studentIdAsNum = Integer.parseInt(studentIDString);
+        } catch (NumberFormatException nfe){
+            containsChar = true;
+            //if the input contains other characters display error
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid ID type");
+            alert.setHeaderText("Input contains characters\n Enter your 10-digit ASU ID to authenticate");
+            alert.showAndWait();
+        }
+
+        //checking if the entered ID is in the database array
+        checkVal = false;
+        System.out.println(checkVal);
+        for (int i = 0; i < studentIdDatabase.length; i++){
+
+            if(studentIdDatabase[i]==studentIdAsNum){
+                checkVal  = true;
+            }
+        }
+        System.out.println("The value after the for loop: " + checkVal);
+        
+        //if the ID is authenticated move to pizza ordering screen
+        if(checkVal){
         root = FXMLLoader.load(getClass().getResource("PizzaSelection.fxml")); 
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        } else if (!containsChar){ //if the ID is not in the database AND contains only integers, display error and wait for new input
+
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid ASU ID");
+            alert.setHeaderText("Your 10-digit input is not an ASU ID\n Please try again with a valid ASU ID");
+            alert.showAndWait();
+            
+        }
 
     }
 
-    public void switchToScene2(ActionEvent event) throws IOException {
+    //**************************************************************************
+    //if the chef side "next" button is clicked
+    //check if ID is entered, is a 10 digit integer and is in the chef database
+    @FXML 
+    public void goToChef(ActionEvent event) throws IOException {
+
+        boolean checkVal = false; //
+        boolean containsChar = false; //set to true if ID contains characters
+        String cName = chefName.getText();
+        int chefIdAsNum = 0;
+        String chefIDString = chefIDEntered.getText();
+
+        System.out.println(cName);
+        System.out.println(chefIDString);
+
+        try{ //parse the string input from password field to an int
+            chefIdAsNum = Integer.parseInt(chefIDString);
+        } catch (NumberFormatException nfe){
+            containsChar = true;
+            //if the input contains other characters display error
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid ID type");
+            alert.setHeaderText("Input contains characters\n Enter your 10-digit Chef ID to authenticate");
+            alert.showAndWait();
+        }
+
+        //checking if the entered ID is in the database array
+        checkVal = false;
+        System.out.println(checkVal);
+        for (int i = 0; i < chefIdDatabase.length; i++){
+
+            if(chefIdDatabase[i]==chefIdAsNum){
+                checkVal  = true;
+            }
+        }
+        System.out.println("The value after the for loop: " + checkVal);
+        
+        //if the ID is authenticated move to pizza ordering screen
+        if(checkVal){
         root = FXMLLoader.load(getClass().getResource("ChefTrack.fxml")); 
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        } else if (!containsChar){ //if the ID is not in the database AND contains only integers, display error and wait for new input
+
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Invalid Chef ID");
+            alert.setHeaderText("Your 10-digit input is not a Chef ID\n Please try again with a valid Chef ID");
+            alert.showAndWait();
+            
+        }
 
     }
 
